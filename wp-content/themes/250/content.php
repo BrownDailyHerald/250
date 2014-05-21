b3d77267181baa94ef186be52e26f7dac5859bb3
Joe Stein <joeaarons@gmail.com>
2014-05-21 19:15:21 -0400

    Merged 123's theme. If they complain remind me to send them really passive-aggressive emails

--- wp-content/themes/250/content.php
+++ wp-content/themes/250/content.php
@@ -12,30 +12,37 @@
 $custom_author = get_post_meta( get_the_ID(), '_custom_author', true );
 if (!empty($custom_author)) trim($custom_author);
 ?>
+<div id="main" class="site-main">
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+      <h1 class="entry-title"><?php the_title( ); ?></h1>
+      <div class="entry-meta">
+        <span class="byline">
+          <span class="author vcard">
+            <a class="url fn n" rel="author"><?php if (empty($custom_author)) { the_author(); } else { echo $custom_author; } ?></a>
+          </span>
+        </span>
+      </div>
+    </header>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <header class="entry-header">
-    <h1><?php the_title( ); ?></h1>
-    <span> By: <?php if (empty($custom_author)) { the_author(); } else { echo $custom_author; } ?> </span>
-  </header>
+	  <?php if ( is_search() ) : ?>
+	  <div class="entry-summary">
+		  <?php the_excerpt(); ?>
+	  </div><!-- .entry-summary -->
+	  <?php else : ?>
+	  <div class="entry-content">
+		  <?php
+			  the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', '250' ) );
+			  wp_link_pages( array(
+				  'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', '250' ) . '</span>',
+				  'after'       => '</div>',
+				  'link_before' => '<span>',
+				  'link_after'  => '</span>',
+			  ) );
+		  ?>
+	  </div><!-- .entry-content -->
+	  <?php endif; ?>
 
-	<?php if ( is_search() ) : ?>
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-	<?php else : ?>
-	<div class="entry-content">
-		<?php
-			the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', '250' ) );
-			wp_link_pages( array(
-				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', '250' ) . '</span>',
-				'after'       => '</div>',
-				'link_before' => '<span>',
-				'link_after'  => '</span>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-	<?php endif; ?>
-
-	<?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
-</article><!-- #post-## -->
+	  <?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
+  </article><!-- #post-## -->
+</div>

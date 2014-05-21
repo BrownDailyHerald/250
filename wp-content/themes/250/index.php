a5503aa40341d81a5c9feb8f235d58e0af04953e
Cody Ma <cody_ma@brown.edu>
2014-05-21 11:59:56 -0400

    add archive section

--- wp-content/themes/250/index.php
+++ wp-content/themes/250/index.php
@@ -21,26 +21,57 @@ function render_category($slug = null) {
 <div class="catcontain">
   <div class="category" style="background-color: #dd1e36;">
     <div class="title" style="color: #ffffff"><b>The People</b></div>
+    <div class="archive"></div>
     <div class="photo">
       <?php render_category('the-people'); ?>
-      <img src="http://placekitten.com/1300/600" alt="">
     </div>
   </div>
-  <div class="category" style="background-color: #3a1e1a; margin: auto; z-index: 2000;">
+  <div class="category" style="background-color: #3a1e1a; margin: auto;">
     <div class="title" style="color: #ffffff"><b>The Place</b></div>
+    <div class="content-container">
+      <div class="archive">
+        <h1> <u> Archives </u></h1>
+        <h3> Announcements </h3>
+        <ul>
+          <li> <a href="#"> Article 1 </a> </li>
+          <li> <a href="#"> Article 2 </a> </li>
+        </ul>
+        <h3> Presidency </h3>
+        <ul>
+          <li> <a href="#"> Fuck 1 </a> </li>
+          <li> <a href="#"> Fuck 2 </a> </li>
+        </ul>
+        <h3> Deaths </h3>
+        <ul>
+          <li> <a href="#"> Fuck 1 </a> </li>
+          <li> <a href="#"> Fuck 2 </a> </li>
+        </ul>
+      </div>
+      <div class="content">
+        <h1> <u> Shit load of links...? </u> </h1>
+        <ul>
+          <li> <a href="#"> Fuck 1 </a> </li>
+          <li> <a href="#"> Fuck 2 </a> </li>
+          <li> <a href="#"> Fuck 3 </a> </li>
+          <li> <a href="#"> Fuck 4 </a> </li>
+          <li> <a href="#"> Fuck 5 </a> </li>
+          <li> <a href="#"> Fuck 6 </a> </li>
+        </ul>
+      </div>
+    </div>
     <div class="photo">
       <?php render_category('the-place'); ?>
-      <img src="http://placekitten.com/1300/600" alt="">
     </div>
   </div>
   <div class="category" style="background-color: #dd1e36">
     <div class="title" style="color: #ffffff"><b>The Moment</b></div>
+    <div class="archive"></div>
     <div class="photo">
       <?php render_category('the-moment'); ?>
-      <img src="http://placekitten.com/1300/600" alt="">
     </div>
   </div>
 </div>
+
 <script type='text/javascript'>
   var low_opc = 0.4;
   var min_width = "5%";
